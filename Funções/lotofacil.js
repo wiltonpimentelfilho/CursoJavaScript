8583fa1c578960fa55b426c2dc3787de2510db48
wiltonpimentelfilho <wiltonpimentelfilho@gmail.com>
2022-05-30 14:12:11 -0300

    funções

---
+++ Funções/lotofacil.js
@@ -0,0 +1,14 @@
+
+var vetor = [];
+
+function lotofacil() {
+  
+    for(i=0; i < 15; i++){     
+        sorteio = Math.floor((Math.random() * 25) + 1); //Retorna um número aleatório entre 1 e 25
+        console.log(sorteio);
+
+    //    while(vetor.indexOf(sorteio) >= 0);
+    //    vetor.push(sorteio);
+    }
+}
+lotofacil();

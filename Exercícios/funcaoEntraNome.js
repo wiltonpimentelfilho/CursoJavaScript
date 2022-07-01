bf64cdaa9718f2f2431ca8d3703bd147fc2a5306
wiltonpimentelfilho <wiltonpimentelfilho@gmail.com>
2022-07-01 14:26:44 -0300

    exercícios e projeto

--- Exercícios/funcaoEntraNome.js
+++ Exercícios/funcaoEntraNome.js
@@ -1,10 +1,12 @@
+const prompt = require('prompt-sync')(); //Chamando o pacote prompt-sysnc para poder executar o código no terminal.
+                                         //Sem ele, teria que chamar a função em uma página HTML.//
 
 var nome = prompt('Entre com seu nome: ');
 //var nome = 'Wilton'
 
 function mostra(){
     console.log('Olá, ' + nome + '!')
-    document.write('Olá, ' + nome + '!')
+//    document.write('Olá, ' + nome + '!')
     return
 }
 mostra();
